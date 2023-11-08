b1ec4f0746b56cd35486fc054157fd4f8d097164
ProfL3501 <ProfL3501@lab.aca>
2023-11-08 17:57:05 -0500

    Movimiento de camara CASI terminado

--- Assets/Code/Scripts/PlayerCamera.cs
+++ Assets/Code/Scripts/PlayerCamera.cs
@@ -8,8 +8,12 @@ namespace PlayerCamera
     {
         [SerializeField]
         private float m_MovementSpeed;
+        [SerializeField][Range(0,1)]
+        private float m_MovementInterp;
         [SerializeField]
         private float m_RotationSpeed;
+        [SerializeField][Range(0,1)]
+        private float m_RotationInterp;
         [SerializeField]
         private float m_ZoomSpeed;
 
@@ -17,8 +21,67 @@ namespace PlayerCamera
         private float m_ZoomMax;
         [SerializeField]
         private float m_ZoomMin;
+        [SerializeField][Range(0,1)]
+        private float m_ZoomInterp;
+        [SerializeField]
+        private Transform m_MyCamera;
 
+        private Vector3 m_MovementDesired;
+        private Quaternion m_RotationDesired;
         private float m_ZoomDesired;
+
+        private void Awake() 
+        {
+            MovementDesired = transform.position;
+            RotationDesired = transform.rotation;
+            ZoomDesired = 0f;
+        }
+
+        public float MovementSpeed { 
+            get => m_MovementSpeed; private set => m_MovementSpeed = value; 
+        }
+        public Vector3 MovementDesired { 
+            get => m_MovementDesired; set => m_MovementDesired = value; 
+        }
+        public float RotationSpeed { 
+            get => m_RotationSpeed; private set => m_RotationSpeed = value; 
+        }
+        public Quaternion RotationDesired { 
+            get => m_RotationDesired; set => m_RotationDesired = value; 
+        }
+        public float ZoomSpeed { 
+            get => m_ZoomSpeed; private set => m_ZoomSpeed = value; 
+        }
+        public float ZoomDesired { 
+            get => m_ZoomDesired; set => m_ZoomDesired = value; 
+        }
+
+        private void Update() 
+        {
+            //Calculamos movimiento
+            transform.position = Vector3.Lerp(
+                transform.position,
+                m_MovementDesired,
+                m_MovementInterp
+            );
+
+            // Calculamos rotacion
+            transform.rotation = Quaternion.Lerp(
+                transform.rotation,
+                m_RotationDesired,
+                m_RotationInterp
+            );
+
+            // Calculamos zoom
+            var newPositionY = Mathf.Clamp(transform.position.y + ZoomDesired, m_ZoomMin, m_ZoomMax);
+            
+            var newPosition = m_MyCamera.forward * newPositionY + transform.position;
+            transform.position = Vector3.Lerp(
+                transform.position,
+                newPosition,
+                m_ZoomInterp
+            );
+        }
     }
 
 }
--- Assets/Code/Scripts/PlayerController.cs
+++ Assets/Code/Scripts/PlayerController.cs
@@ -3,25 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+namespace PlayerCamera
 {
-    private void OnMove(InputValue value)
+    public class PlayerController : MonoBehaviour
     {
-        
-    }
+        [SerializeField]
+        private PlayerCamera m_PlayerCamera;
 
-    private void OnZoom(InputValue value)
-    {
+        private Vector2 m_MoveDir;
 
-    }
+        private void OnMove(InputValue value)
+        {
+            m_MoveDir = value.Get<Vector2>().normalized;
+        }
 
-    private void OnRotateRight(InputValue value)
-    {
+        private void OnZoom(InputValue value)
+        {
+            var valScroll = value.Get<float>();
 
-    }
+            if (valScroll != 0f)
+            {
+                m_PlayerCamera.ZoomDesired = Mathf.Sign(valScroll) * m_PlayerCamera.ZoomSpeed;
+            }
+        }
 
-    private void OnRotateLeft(InputValue value)
-    {
-        
+        private void OnRotateRight(InputValue value)
+        {
+            if (value.isPressed)
+            {
+                var rot = Quaternion.Euler(0f, m_PlayerCamera.RotationSpeed, 0f);
+                m_PlayerCamera.RotationDesired *= rot;
+            }
+        }
+
+        private void OnRotateLeft(InputValue value)
+        {
+            if (value.isPressed)
+            {
+                var rot = Quaternion.Euler(0f, -m_PlayerCamera.RotationSpeed, 0f);
+                m_PlayerCamera.RotationDesired *= rot;
+            }
+        }
+
+        private void Update() 
+        {
+            if (m_MoveDir.magnitude != 0f)
+            {
+                var dir = m_PlayerCamera. MovementSpeed * Time.deltaTime * m_MoveDir;
+                m_PlayerCamera.MovementDesired += (
+                    m_PlayerCamera.transform.forward * dir.y + 
+                    m_PlayerCamera.transform.right * dir.x
+                );
+            }
+        }
     }
 }
